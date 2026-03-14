8712050acbcf1e87c13d055f4a8d6423b015f286
Nuno Costa <nuno.pinto.costa@gmail.com>
2026-03-14 11:10:31 +0000

    Refactor Ball_Behaviour animation logic
    Rewrite Ball_Behaviour to improve animator control and prevent overlapping animations: use explicit animator triggers (LookLeft/Right/Up/Down, Tired, RollingEyes, Blink), add timing/cooldowns (MinAnimationInterval, RollingEyesCooldown), reset one-shot triggers, and centralize animation state handling with coroutines. Also switch startup state to Idle, use Animator on the GameObject, and simplify collision/trigger handling. Additionally add EyelidMask.mask (and meta), update BallCleanup.fbx.meta to enable avatarSetup, and update the BallAnimatorController binary asset.

--- Assets/Scripts/Ball/Ball_Behaviour.cs
+++ Assets/Scripts/Ball/Ball_Behaviour.cs
@@ -1,158 +1,232 @@
 using UnityEngine;
 using System.Collections;
 
-public class Ball_Behaviour : MonoBehaviour {
+public class Ball_Behaviour : MonoBehaviour
+{
+    protected bool game_restarted = true;
+    protected bool animation_finished = true;
+    protected bool rolling_eyes = false;
+    protected bool tired_active = false;
 
-	protected bool game_restarted = true;
-	protected bool animation_finished = true;
-	protected bool rolling_eyes = false;
+    private int current_area = 7;
+    private float last_animation_time = -999f;
+    private const float MinAnimationInterval = 2f;
+    private const float RollingEyesCooldown = 4f;
+    private float last_rolling_eyes_time = -999f;
 
-	private int current_area = 7;
+    protected GameObject last_player_touched;
+    protected bool is_looking_somewhere;
+    protected Animator animator;
 
-	string[] animationsType1;
-	string[] animationsType2;
+    private const string ParamBlink = "Blink";
+    private const string ParamTired = "Tired";
+    private const string ParamRollingEyes = "RollingEyes";
+    private const string ParamLookLeft = "LookLeft";
+    private const string ParamLookRight = "LookRight";
+    private const string ParamLookUp = "LookUp";
+    private const string ParamLookDown = "LookDown";
 
-	protected GameObject last_player_touched;
+    protected void Start()
+    {
+        is_looking_somewhere = false;
+        animator = GetComponent<Animator>();
 
-	protected bool is_looking_somewhere;
+        GameObject[] center_planes = GameObject.FindGameObjectsWithTag("center-plane");
+        GameObject center_circle_left = GameObject.FindGameObjectWithTag("center-circle-left");
+        GameObject center_circle_right = GameObject.FindGameObjectWithTag("center-circle-right");
 
-	protected Animator animator;
+        for (int i = 0; i < center_planes.Length; i++)
+        {
+            Physics.IgnoreCollision(center_planes[i].GetComponent<Collider>(), transform.GetComponent<Collider>());
+        }
 
-	// Animator parameter names (must match your Animator Controller)
-	const string ParamBlink = "Blink";
-	const string ParamTired = "Tired";
-	const string ParamRollingEyes = "RollingEyes";
-	const string ParamRandomIdle = "RandomIdle";
+        Physics.IgnoreCollision(center_circle_left.GetComponent<Collider>(), transform.GetComponent<Collider>());
+        Physics.IgnoreCollision(center_circle_right.GetComponent<Collider>(), transform.GetComponent<Collider>());
 
-	public void GameHasRestarted()
-	{
-		game_restarted = true;
-	}
+        if (animator != null)
+        {
+            animator.Play("Idle", 0, 0f);
+        }
 
-	void OnCollisionEnter(Collision collider)
-	{
-		if (collider.gameObject.tag == "forcefield") {
-			CourtCollision(collider.contacts[0].point);
-		} else {
-			ReleasePlayers();
-		}
-	}
+        rolling_eyes = false;
+        tired_active = false;
+        animation_finished = true;
+        last_animation_time = Time.time;
+    }
 
-	protected virtual void CourtCollision(Vector3 point)
-	{
-		Forcefield forcefield = GameObject.FindGameObjectWithTag("forcefield").GetComponent<Forcefield>();
-		forcefield.BallCollition(point);
-		int random = Random.Range(0, 100);
-		if (random <= 10) {
-			if (!rolling_eyes && animator != null) {
-				StopAllCoroutines();
-				rolling_eyes = true;
-				animation_finished = true;
-				animator.SetTrigger(ParamRollingEyes);
-				StartCoroutine(SetRollingEyesFalseAfterDelay(5f));
-			}
-		}
-	}
+    protected void Update()
+    {
+        if (animator == null) return;
 
-	public void SetCurrentArea(int area)
-	{
-		current_area = area;
-	}
+        float now = Time.time;
 
-	public int GetCurrentArea()
-	{
-		return current_area;
-	}
+        if (now - last_animation_time < MinAnimationInterval) return;
+        if (rolling_eyes || !animation_finished) return;
 
-	protected void OnTriggerEnter(Collider collider)
-	{
-		if (collider.gameObject.tag == "colliderShoot")
-			last_player_touched = collider.gameObject.transform.parent.gameObject;
-	}
+        int rand = Random.Range(0, 100);
 
-	public GameObject GetLastPlayerTouched()
-	{
-		return last_player_touched;
-	}
+        if (rand < 1)
+        {
+            PlayTired(now);
+        }
+        else if (rand < 50)
+        {
+            PlayRandomLook(now);
+        }
+        else if (rand < 55)
+        {
+            TryBlink(now);
+        }
+    }
 
-	public void SetLastPlayerTouched(GameObject player)
-	{
-		last_player_touched = player;
-	}
+    private void PlayTired(float now)
+    {
+        ResetOneShotTriggers();
+        animator.SetTrigger(ParamTired);
 
-	public void OnCollisionExit(Collision collider)
-	{
-		if (collider.gameObject.tag == "Player")
-			last_player_touched = collider.gameObject;
-	}
+        tired_active = true;
+        animation_finished = false;
+        last_animation_time = now;
 
-	IEnumerator SetAnimationFinishedAfterDelay(float delay)
-	{
-		yield return new WaitForSeconds(delay);
-		animation_finished = true;
-	}
+        StartCoroutine(ClearMainAnimationAfterDelay(2.5f, true));
+    }
 
-	IEnumerator SetRollingEyesFalseAfterDelay(float delay)
-	{
-		yield return new WaitForSeconds(delay);
-		rolling_eyes = false;
-	}
+    private void PlayRandomLook(float now)
+    {
+        ResetOneShotTriggers();
 
-	public void ReleasePlayers()
-	{
-		if (game_restarted)
-		{
-			GameObject gbo = GameObject.FindGameObjectWithTag("GameController");
-			Game_Behaviour gb = gbo.GetComponent<Game_Behaviour>();
-			gb.ReleasePlayers();
-			game_restarted = false;
-		}
-	}
+        int dir = Random.Range(0, 4);
+        switch (dir)
+        {
+            case 0: animator.SetTrigger(ParamLookLeft); break;
+            case 1: animator.SetTrigger(ParamLookRight); break;
+            case 2: animator.SetTrigger(ParamLookUp); break;
+            case 3: animator.SetTrigger(ParamLookDown); break;
+        }
 
-protected void Start()
-	{
-		is_looking_somewhere = false;
-		animator = GetComponentInChildren<Animator>();
+        animation_finished = false;
+        last_animation_time = now;
 
-		animationsType1 = new string[] { "look_left", "look_right", "look_up", "look_down" };
-		animationsType2 = new string[] { "Default" };
+        StartCoroutine(ClearMainAnimationAfterDelay(2.5f, false));
+    }
 
-		GameObject[] center_planes = GameObject.FindGameObjectsWithTag("center-plane");
-		GameObject center_circle_left = GameObject.FindGameObjectWithTag("center-circle-left");
-		GameObject center_circle_rigth = GameObject.FindGameObjectWithTag("center-circle-right");
+    private void TryBlink(float now)
+    {
+        if (rolling_eyes) return;
+        if (tired_active) return;
 
-		for (int i = 0; i < center_planes.Length; i++)
-			Physics.IgnoreCollision(center_planes[i].GetComponent<Collider>(), transform.GetComponent<Collider>());
-		Physics.IgnoreCollision(center_circle_left.GetComponent<Collider>(), transform.GetComponent<Collider>());
-		Physics.IgnoreCollision(center_circle_rigth.GetComponent<Collider>(), transform.GetComponent<Collider>());
+        animator.ResetTrigger(ParamBlink);
+        animator.SetTrigger(ParamBlink);
+        last_animation_time = now;
+    }
 
-		//if (animator != null)
-	//	{
-			// Ensure we start in the Default state at time 0
-			animator.Play("Default", 0, 0f);
-		//}
+    private void ResetOneShotTriggers()
+    {
+        animator.ResetTrigger(ParamLookLeft);
+        animator.ResetTrigger(ParamLookRight);
+        animator.ResetTrigger(ParamLookUp);
+        animator.ResetTrigger(ParamLookDown);
+        animator.ResetTrigger(ParamTired);
+        animator.ResetTrigger(ParamRollingEyes);
+    }
 
-		animation_finished = true;
-	}
+    private IEnumerator ClearMainAnimationAfterDelay(float delay, bool clearTired)
+    {
+        yield return new WaitForSeconds(delay);
+        animation_finished = true;
 
-	protected void Update()
-	{
-		//if (animator == null) return;
+        if (clearTired)
+            tired_active = false;
+    }
 
-		if (!rolling_eyes && animation_finished) {
-			int rand = Random.Range(0, 1000);
-			if (rand < 1) {
-				animator.SetTrigger(ParamTired);
-				animation_finished = false;
-				StartCoroutine(SetAnimationFinishedAfterDelay(2.5f));
-			} else if (rand < 10) {
-				animator.SetInteger(ParamRandomIdle, Random.Range(0, 4));
-				animation_finished = false;
-				StartCoroutine(SetAnimationFinishedAfterDelay(2.5f));
-			}
-		}
+    protected virtual void CourtCollision(Vector3 point)
+    {
+        Forcefield forcefield = GameObject.FindGameObjectWithTag("forcefield").GetComponent<Forcefield>();
+        forcefield.BallCollition(point);
 
-		if (Random.Range(0, 100) == 0)
-			animator.SetTrigger(ParamBlink);
-	}
+        float now = Time.time;
+        if (now - last_rolling_eyes_time < RollingEyesCooldown) return;
+
+        int random = Random.Range(0, 100);
+        if (random <= 10 && !rolling_eyes && animator != null)
+        {
+            rolling_eyes = true;
+            animation_finished = false;
+            tired_active = false;
+            last_rolling_eyes_time = now;
+            last_animation_time = now;
+
+            ResetOneShotTriggers();
+            animator.SetTrigger(ParamRollingEyes);
+
+            StartCoroutine(EndRollingEyesAfterDelay(5f));
+        }
+    }
+
+    private IEnumerator EndRollingEyesAfterDelay(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        rolling_eyes = false;
+        animation_finished = true;
+    }
+
+    public void GameHasRestarted()
+    {
+        game_restarted = true;
+    }
+
+    void OnCollisionEnter(Collision collider)
+    {
+        if (collider.gameObject.CompareTag("forcefield"))
+        {
+            CourtCollision(collider.contacts[0].point);
+        }
+        else
+        {
+            ReleasePlayers();
+        }
+    }
+
+    protected void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.CompareTag("colliderShoot"))
+            last_player_touched = collider.gameObject.transform.parent.gameObject;
+    }
+
+    public GameObject GetLastPlayerTouched()
+    {
+        return last_player_touched;
+    }
+
+    public void SetLastPlayerTouched(GameObject player)
+    {
+        last_player_touched = player;
+    }
+
+    public void OnCollisionExit(Collision collider)
+    {
+        if (collider.gameObject.CompareTag("Player"))
+            last_player_touched = collider.gameObject;
+    }
+
+    public void ReleasePlayers()
+    {
+        if (game_restarted)
+        {
+            GameObject gbo = GameObject.FindGameObjectWithTag("GameController");
+            Game_Behaviour gb = gbo.GetComponent<Game_Behaviour>();
+            gb.ReleasePlayers();
+            game_restarted = false;
+        }
+    }
+
+    public void SetCurrentArea(int area)
+    {
+        current_area = area;
+    }
+
+    public int GetCurrentArea()
+    {
+        return current_area;
+    }
 }
